bee437c27011e8a2ea15234c9b701d2f4be264a5
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-07-07 15:38:00 +0200

    reply[:out] is enough

--- plugins/mcollective/agent/puppetnode.rb
+++ plugins/mcollective/agent/puppetnode.rb
@@ -3,12 +3,6 @@ module MCollective
     class Puppetnode<RPC::Agent
       action "deactivate" do
         reply[:out] = %x[puppet node deactivate #{request[:node]}]
-
-        if reply[:out] =~ /disabled/
-          reply[:disabled] = :true
-        else
-          reply[:disabled] = :false
-        end
       end
     end
   end
